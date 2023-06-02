ad4837caf8e0206e94ed7a86da2cb73b1937dc17
Daniel Lerps <contact@lerps.de>
2023-06-02 14:05:36 +0200

    Enable env logging control

--- Dynamo.Worker/Logging/LoggerFactory.cs
+++ Dynamo.Worker/Logging/LoggerFactory.cs
@@ -1,6 +1,7 @@
 using Serilog;
 using Serilog.Core;
 using Serilog.Exceptions;
+using Serilog.Formatting.Compact;
 
 namespace Dynamo.Worker.Logging;
 
@@ -8,9 +9,22 @@ public static class LoggerFactory
 {
     public static Logger CreateLogger()
     {
-        return new LoggerConfiguration()
-            .MinimumLevel.Debug()
-            .WriteTo.Console()
+        var envDebug = Environment.GetEnvironmentVariable("Verbose");
+        var envJsonFormat = Environment.GetEnvironmentVariable("JsonLogFormat");
+
+        var config = new LoggerConfiguration();
+
+        if (envDebug != null && envDebug.Equals("true", StringComparison.OrdinalIgnoreCase))
+            config = config.MinimumLevel.Debug();
+        else
+            config = config.MinimumLevel.Information();
+
+        if (envJsonFormat != null && envJsonFormat.Equals("true", StringComparison.OrdinalIgnoreCase))
+            config = config.WriteTo.Console(new CompactJsonFormatter());
+        else
+            config = config.WriteTo.Console();
+
+        return config
             .WriteTo.Debug()
             .Enrich.FromLogContext()
             .Enrich.WithExceptionDetails()
